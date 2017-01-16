8552a24cd8f13317464d88cdcafad2011c878f0d
localadmin <dskha@allstate.com>
2017-01-16 16:17:46 +0530

    Project real time

--- src/main/resources/db/migration/V1__create_initial_schema.sql
+++ src/main/resources/db/migration/V1__create_initial_schema.sql
@@ -1,4 +1,4 @@
-CREATE TABLE `bollywooddev`.`movies` (
+CREATE TABLE `movies` (
   `id`        INT          NOT NULL    AUTO_INCREMENT,
   `version`   INT          NOT NULL    DEFAULT 0,
   `title`     VARCHAR(255) NOT NULL,
